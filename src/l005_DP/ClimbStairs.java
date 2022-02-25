42510a5680f80927b882284c5fafbadc8d171252
rahulanand007 <rahul03061998@gmail.com>
2022-02-25 18:06:11 +0530

    commit

--- src/l005_DP/ClimbStairs.java
+++ src/l005_DP/ClimbStairs.java
@@ -1,8 +1,29 @@
 package l005_DP;
+import java.util.*;
 
 public class ClimbStairs {
     
     public static void main(String[] args){
-
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        climbStairs(n, new int[n+1]);
+        scn.close();
     }
+
+    public static int climbStairs(int n, int[] qb){
+        if(n==0){ 
+            return 1;
+        }else if(n<0){
+            return 0;
+        }
+        if(qb[n]!=0){
+            return qb[n];
+        }
+        int p1 =climbStairs(n-1, qb);
+        int p2 =climbStairs(n-2, qb);
+        int p3 =climbStairs(n-3, qb);
+
+        qb[n] = p1 + p2 + p3;
+        return p1 + p2 +p3;
+    }  
 }
